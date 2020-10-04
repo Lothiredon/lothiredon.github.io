2059fe590852d5b827eb4ba4985be6af90fe7487
Lothiredon <lothiredon@gmail.com>
2020-10-04 03:16:58 +0100

    Shop v1.3
    JS partial fix

--- scripts/cart-curtain.js
+++ scripts/cart-curtain.js
@@ -1,5 +1,5 @@
 /* Open when someone clicks on the span element */
-window.onload=function(){
+/*window.onload=function(){
 
     var curtainOpen = 1
     var $notoverlay = $("div:not(.overlay-content)")
@@ -12,24 +12,35 @@ window.onload=function(){
     /* Close when someone clicks on the "x" symbol inside the overlay
     function closeNav() {
     document.getElementById("cart-curtain").style.width = "0%";
-    }*/
+    }
     $notoverlay.click(function(e) {
         if(curtainOpen == 0) {
                 e.stopPropagation();
                 document.getElementById("cart-curtain").style.width = "0%";
                 curtainOpen++
-                /*alert(curtainOpen);*/
+                /*alert(curtainOpen);
             }
         else if(curtainOpen >= 1) {
             e.stopPropagation();
             document.getElementById("cart-curtain").style.width = "100%";
             curtainOpen--
-            /*alert(curtainOpen);*/
+            /*alert(curtainOpen);
 
         }
         else {
-            /*alert(curtainOpen);*/
+            /*alert(curtainOpen);
             return;
         }
     });
-}
+}*/
+
+window.onclick = e => {
+    if($(e.target).hasClass("overlay")) {
+      // Do close stuff
+        document.getElementById("cart-curtain").style.width = "0%";
+    }
+    else if($(e.target).hasClass("shopping-cart" || "closebtn")) {
+      // Do open staff
+        document.getElementById("cart-curtain").style.width = "25%";
+    }
+  };
